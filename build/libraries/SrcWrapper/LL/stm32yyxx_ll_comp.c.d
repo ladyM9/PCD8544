1a566f3b5914e5da267ebfcbcf1dcd0cebbb3e50
Borna Biro <43830307+BornaBiro@users.noreply.github.com>
2024-09-17 21:50:50 +0200

    Removed build and .vscode files.

--- build/libraries/SrcWrapper/LL/stm32yyxx_ll_comp.c.d
+++
@@ -1,2 +0,0 @@
-D:\Moji\ projekti\PCD8544\build\libraries\SrcWrapper\LL\stm32yyxx_ll_comp.c.o: \
- C:\Users\Marina\AppData\Local\Arduino15\packages\STMicroelectronics\hardware\stm32\2.7.1\libraries\SrcWrapper\src\LL\stm32yyxx_ll_comp.c

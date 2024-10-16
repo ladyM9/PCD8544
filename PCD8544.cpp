fdd0768c9dcfc34a753300c5a0134fd556fc8661
ladyM9 <marinagolic12@gmail.com>
2024-10-16 18:37:39 +0200

    The display cannot be cleared

--- PCD8544.cpp
+++ PCD8544.cpp
@@ -19,16 +19,17 @@ void PCD8544_LCD::begin(int _SCEPIN, int _DCPIN, int _RSTPIN)
   pinMode(_DC, OUTPUT);
   pinMode(_RST, OUTPUT);
   SPI.begin();
-  LCDreset();
- // digitalWrite(_SCE, HIGH); // CS pin must be HIGH when the SPI can be initialized
+  
+  //digitalWrite(_SCE, HIGH); // CS pin must be HIGH when the SPI can be initialized
   digitalWrite(_DC, LOW);
   uint8_t function_s = PCD8544_LCD_CMD_FS1; //send function set to spi
   spi_send(&function_s, 1);
   uint8_t function_s1 = PCD8544_LCD_CMD_FS2;
   spi_send(&function_s1,1);
+  LCDreset();
 
 
-   digitalWrite(_DC, HIGH);
+   digitalWrite(_SCE, LOW);
 
   display();
 
@@ -43,7 +44,7 @@ void PCD8544_LCD::display()
   {
     digitalWrite(_DC, LOW); //_DC must be LOW(datasheet)
 
-    uint8_t display_control = PCD8544_LCD_CMD_DISPC;
+    uint8_t display_control = PCD8544_LCD_CMD_DISP_NORMAL_MODE;
     spi_send(&display_control, 1);
 
     uint8_t set_Y = PCD8544_LCD_CMD_SETY;
@@ -62,6 +63,9 @@ void PCD8544_LCD::display()
 void PCD8544_LCD::initLCD()
 {
 
+  for (int i = 0; i < 8; i++)
+  {
+
   uint8_t bias = PCD8544_LCD_CMD_BS;
   spi_send(&bias,1);
 
@@ -70,10 +74,7 @@ void PCD8544_LCD::initLCD()
 
   uint8_t vop = PCD8544_LCD_CMD_VOP;
   spi_send(&vop,1);
-
-  
-
-
+}
 }
 
 void PCD8544_LCD::drawPixel(int16_t x, int16_t y, uint16_t color)
@@ -99,17 +100,18 @@ void PCD8544_LCD::spi_send(uint8_t *_data, uint16_t _n)
   
   SPI.endTransaction();
   digitalWrite(_SCE, HIGH);
+  digitalWrite(_SCE, LOW);
 }
 
 void PCD8544_LCD::LCDreset()
 {
   digitalWrite(_RST, LOW);
 
-  delay(100);
+  delay(500);
 
   digitalWrite(_RST, HIGH);
 
-  delay(100);
+  delay(500);
 }
 
 
@@ -120,7 +122,10 @@ void PCD8544_LCD::Contrast(uint8_t contrast)
 
 void PCD8544_LCD::clearDisplay()
 {
-  memset(_lcdFramebuffer, 0x00, sizeof(_lcdFramebuffer));
+ 
+  memset(_lcdFramebuffer , 0X01, width*height/8);
+  delay(3000);
+   digitalWrite(_DC, HIGH);
  // memset(buffer, color ? 0xFF : 0x00, bytes);
 }
 
--- PCD8544.ino
+++ PCD8544.ino
@@ -9,12 +9,17 @@ PCD8544_LCD display_lcd;
 
 void setup()
 {
+	
 	display_lcd.begin(_SCEPIN , _DCPIN, _RSTPIN);
-	//display_lcd.setTextColor(0xFFFFFF, 0x000000);
 
-	display_lcd.display();
+	
+
+	//display_lcd.display();
 	delay(5000);
+
 	display_lcd.clearDisplay();
+	delay(5000);
+	display_lcd.display();
 
 
 
@@ -22,7 +27,8 @@ void setup()
 
 void loop()
 {
-	display_lcd.drawPixel(5,5,1);
-	display_lcd.display();
+	//display_lcd.drawPixel(5,5,1);
+	//display_lcd.display();
+
 
 }
